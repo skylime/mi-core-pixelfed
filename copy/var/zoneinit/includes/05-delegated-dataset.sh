90276f28591bc77eaf8759e8a0499cfd1b7ec35d
Thomas Merkel <tm@core.io>
2025-01-26 23:19:25 +0100

    continue

---
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if DDS=$(/opt/core/bin/dds); then
+  zfs create "${DDS}/pixelfed_storage" || true
+  zfs create "${DDS}/mysql" || true
+
+  if ! zfs get -o value -H mountpoint "${DDS}/pixelfed_storage" | grep -q /opt/pixelfed/storage; then
+    zfs set mountpoint=/opt/pixelfed/storage "${DDS}/pixelfed_storage"
+  fi
+  if ! zfs get -o value -H mountpoint "${DDS}/mysql" | grep -q /var/mysql; then
+    zfs set mountpoint=/var/mysql "${DDS}/mysql"
+  fi
+fi
+
+# Always try to set the correct permissions for /var/mysql
+chown -R mariadb:mariadb /var/mysql || true
+chown -R pixelfed:www /opt/prixelfed/storage || true
--- copy/var/zoneinit/includes/50-pixelfed.sh
+++ copy/var/zoneinit/includes/50-pixelfed.sh
@@ -19,45 +19,62 @@ _insertreplace() {
     "${value:0:1}" != "[" ]]; then
     value="\"${value}\""
   fi
-  if grep -q "${key}" ${file}; then
-    gsed -i "s|^${key} \+=.*|${key} = ${value}|g" ${file}
+  if grep -q "${key}" "${file}"; then
+    gsed -i "s|^${key}\s*=.*|${key}=${value}|g" "${file}"
   else
-    echo "${key} = ${value}" >> ${file}
+    echo "${key} = ${value}" >> "${file}"
   fi
+}
+# _pixelfed_php command
+_pixelfed_php() {
+  cd "${PIXELFED_HOME}" && php ${*}
 }
 
 log "Create database if not exists"
-if [[ ! -d /var/mysql/kumquat ]]; then
-  KUMQUAT_INIT="CREATE DATABASE IF NOT EXISTS kumquat;
-        CREATE USER 'kumquat'@'localhost' IDENTIFIED BY '${MYSQL_KUMQUAT_PW}';
-        CREATE USER 'kumquat'@'127.0.0.1' IDENTIFIED BY '${MYSQL_KUMQUAT_PW}';
-        GRANT ALL PRIVILEGES ON kumquat.* TO 'kumquat'@'localhost';
-        GRANT ALL PRIVILEGES ON kumquat.* TO 'kumquat'@'127.0.0.1';
+if [[ ! -d /var/mysql/pixelfed ]]; then
+  PIXELFED_INIT="CREATE DATABASE IF NOT EXISTS pixelfed;
+        CREATE USER 'pixelfed'@'localhost' IDENTIFIED BY '${MYSQL_PIXELFED_PW}';
+        CREATE USER 'pixelfed'@'127.0.0.1' IDENTIFIED BY '${MYSQL_PIXELFED_PW}';
+        GRANT ALL PRIVILEGES ON pixelfed.* TO 'pixelfed'@'localhost';
+        GRANT ALL PRIVILEGES ON pixelfed.* TO 'pixelfed'@'127.0.0.1';
         FLUSH PRIVILEGES;"
 
-  mysql --user=root --password=${MYSQL_ROOT_PW} -e "${KUMQUAT_INIT}" > /dev/null \
+  mysql --user=root --password=${MYSQL_ROOT_PW} -e "${PIXELFED_INIT}" > /dev/null \
     || (log "ERROR MySQL query failed to execute." && exit 31)
-  DB_CREATED=true
 fi
 
 log "Initial setup .env file"
 if [[ ! -f "${PIXELFED_HOME}/.env" ]]; then
   cp "${PIXELFED_HOME}/.env.example" "${PIXELFED_HOME}/.env"
-
-  PIXELFED_KEY=${PIXELFED_KEY:-$(mdata-get pixelfed_key 2> /dev/null)} \
-    || PIXELFED_KEY=$(php artisan key:generate --show)
-  mdata-put pixelfed_key "${PIXELFED_KEY}"
 fi
+PIXELFED_KEY=${PIXELFED_KEY:-$(mdata-get pixelfed_key 2> /dev/null)} \
+  || PIXELFED_KEY=$(_pixelfed_php artisan key:generate --show)
+mdata-put pixelfed_key "${PIXELFED_KEY}"
 
-_insertreplace ${PIXELFED_ENV} APP_NAME
+if PIXELFED_APP_NAME=$(mdata-get pixelfed_app_name 2> /dev/null); then
+  _insertreplace ${PIXELFED_ENV} APP_NAME "${PIXELFED_APP_NAME}"
+fi
+_insertreplace ${PIXELFED_ENV} APP_KEY "$(mdata-get pixelfed_key)"
 _insertreplace ${PIXELFED_ENV} APP_URL "https://${HOSTNAME}"
-_insertreplace ${PIXELFED_ENV} APP_DOMAIN ${HOSTNAME}
-_insertreplace ${PIXELFED_ENV} ADMIN_DOMAIN ${HOSTNAME}
-_insertreplace ${PIXELFED_ENV} SESSION_DOMAIN ${HOSTNAME}
+_insertreplace ${PIXELFED_ENV} APP_DOMAIN "${HOSTNAME}"
+_insertreplace ${PIXELFED_ENV} ADMIN_DOMAIN "${HOSTNAME}"
+_insertreplace ${PIXELFED_ENV} SESSION_DOMAIN "${HOSTNAME}"
 
-_insertreplace ${PIXELFED_ENV} DB_PASSWORD "TODO"
+_insertreplace ${PIXELFED_ENV} DB_PASSWORD "${MYSQL_PIXELFED_PW}"
 
 _insertreplace ${PIXELFED_ENV} MAIL_DRIVER sendmail
 
 _insertreplace ${PIXELFED_ENV} ACTIVITY_PUB true
 _insertreplace ${PIXELFED_ENV} AP_REMOTE_FOLLOW true
+
+log "Initial commands for setup"
+_pixelfed_php artisan storage:link
+_pixelfed_php artisan migrate --force
+_pixelfed_php artisan import:cities
+_pixelfed_php artisan instance:actor
+_pixelfed_php artisan route:cache
+_pixelfed_php artisan view:cache
+
+log "Install Laravel Horizon"
+_pixelfed_php artisan horizon:install
+_pixelfed_php artisan horizon:publish
